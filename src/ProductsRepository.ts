40471cadf2a195aa33201267b11805b5e1c65575
Capovila <viniciusbuzaneli@gmail.com>
2024-10-28 19:01:09 -0300

    CRUD

--- src/ProductsRepository.ts
+++ src/ProductsRepository.ts
@@ -1,73 +1,70 @@
-import { ResultSetHeader } from "mysql2"
-import { conn } from "./db"
-import { Product } from "./product"
+import { ResultSetHeader } from "mysql2";
+import { conn } from "./db";
+import { Product } from "./product";
 
+//o dump passa 'products' no minusculo, tive que mudar pra funcionar
 export class ProductsRepository {
-
   getAll(): Promise<Product[]> {
     return new Promise((resolve, reject) => {
-      conn.query<Product[]>("SELECT * FROM PRODUCTS", (err, res) => {
-        if (err) reject(err)
-        else resolve(res)
-      })
-    })
+      conn.query<Product[]>("SELECT * FROM products", (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
   }
 
   getById(product_id: number): Promise<Product | undefined> {
     return new Promise((resolve, reject) => {
       conn.query<Product[]>(
-        "SELECT * FROM PRODUCTS WHERE id = ?",
+        "SELECT * FROM products WHERE id = ?",
         [product_id],
         (err, res) => {
-          if (err) reject(err)
-          else resolve(res?.[0])
+          if (err) reject(err);
+          else resolve(res?.[0]);
         }
-      )
-    })
+      );
+    });
   }
 
   create(p: Product): Promise<Product> {
     return new Promise((resolve, reject) => {
       conn.query<ResultSetHeader>(
-        "INSERT INTO PRODUCTS (name, price, description) VALUES(?,?,?)",
+        "INSERT INTO products (name, price, description) VALUES(?,?,?)",
         [p.name, p.price, p.description],
         (err, res) => {
-          if (err) reject(err)
+          if (err) reject(err);
           else
             this.getById(res.insertId)
-              .then(user => resolve(user!))
-              .catch(reject)
+              .then((user) => resolve(user!))
+              .catch(reject);
         }
-      )
-    })
+      );
+    });
   }
 
   update(p: Product): Promise<Product | undefined> {
     return new Promise((resolve, reject) => {
       conn.query<ResultSetHeader>(
-        "UPDATE PRODUCTS SET name = ?, price = ?, description = ? WHERE id = ?",
+        "UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?",
         [p.name, p.price, p.description, p.id],
         (err, res) => {
-          if (err) reject(err)
-          else
-            this.getById(p.id!)
-              .then(resolve)
-              .catch(reject)
+          if (err) reject(err);
+          else this.getById(p.id!).then(resolve).catch(reject);
         }
-      )
-    })
+      );
+    });
   }
 
   delete(product_id: number): Promise<number> {
     return new Promise((resolve, reject) => {
       conn.query<ResultSetHeader>(
-        "DELETE FROM PRODUCTS WHERE id = ?",
+        "DELETE FROM products WHERE id = ?",
         [product_id],
         (err, res) => {
-          if (err) reject(err)
-          else resolve(res.affectedRows)
+          if (err) reject(err);
+          else resolve(res.affectedRows);
         }
-      )
-    })
+      );
+    });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,64 @@
 import express from "express";
 import { Request, Response, Router } from "express";
-import {ProductsRepository} from "./ProductsRepository";
+import { ProductsRepository } from "./ProductsRepository";
+import { Product } from "./product";
 
 const app = express();
 const port = 3000;
 const routes = Router();
 
+//permite passar os parametros por json
+app.use(express.json());
+
 const productsRepo = new ProductsRepository();
 
-routes.get('/', (req: Request, res: Response)=>{
-    res.statusCode = 200;
-    res.send("Funcionando...");
+routes.get("/", (req: Request, res: Response) => {
+  res.statusCode = 200;
+  res.send("Funcionando...");
 });
 
-routes.get('/getAllProducts', async(req: Request, res: Response)=>{
-    // obter todos os produtos.
-    const products = await productsRepo.getAll();
-    res.statusCode = 200; 
-    res.type('application/json')
-    res.send(products);
+routes.get("/getAllProducts", async (req: Request, res: Response) => {
+  // obter todos os produtos.
+  const products = await productsRepo.getAll();
+  res.statusCode = 200;
+  res.type("application/json");
+  res.send(products);
 });
 
-// aplicar as rotas na aplicação web backend. 
+routes.get("/getById/:id", async (rec: Request, res: Response) => {
+  const products = await productsRepo.getById(
+    rec.params.id as unknown as number
+  );
+  res.statusCode = 200;
+  res.type("application/json");
+  res.send(products);
+});
+
+routes.post("/create", async (rec: Request, res: Response) => {
+  const newProduct = rec.body as Product;
+  const products = await productsRepo.create(newProduct);
+  res.statusCode = 200;
+  res.type("application/json");
+  res.send(products);
+});
+
+routes.post("/update", async (rec: Request, res: Response) => {
+  const updatedProduct = rec.body as Product;
+  const products = await productsRepo.update(updatedProduct);
+  res.statusCode = 200;
+  res.type("application/json");
+  res.send(products);
+});
+
+routes.delete("/delete/:id", async (rec: Request, res: Response) => {
+  await productsRepo.delete(rec.params.id as unknown as number);
+  res.statusCode = 204;
+  res.type("application/json");
+});
+
+// aplicar as rotas na aplicação web backend.
 app.use(routes);
 
-app.listen(3000, ()=>{
-    console.log("Server is running on 3000");
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
 });
