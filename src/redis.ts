b7580526ce014ed3a920e904aa965ffbdf4a867f
guilherme-hl1ma <guihlima@outlook.com>
2024-10-30 23:01:34 -0300

    sincronizacao redis mysql

--- src/redis.ts
+++ src/redis.ts
@@ -1,16 +1,88 @@
-import { createClient } from "redis";
+import { createClient, RedisClientType, SchemaFieldTypes } from "redis";
+import { Product } from "./product";
+import { ProductsRepository } from "./ProductsRepository";
 
-const client = createClient();
-client.on("error", (err) => console.log("Redis Client Error", err));
+export class Redis {
+  private client: RedisClientType;
+  private productRepository: ProductsRepository;
 
-const connectRedis = async () => {
-  await client.connect();
-};
+  constructor() {
+    this.client = createClient();
+    this.conn();
+    this.onError();
+    this.productRepository = new ProductsRepository();
+  }
 
-try {
-  connectRedis();
-} catch (e) {
-  console.log(e);
+  async conn(): Promise<RedisClientType> {
+    if (this.client.isOpen) {
+      return this.client;
+    }
+    try {
+      await this.client.connect();
+      return this.client;
+    } catch (error) {
+      throw new Error("Failed to connect to redis: " + error);
+    }
+  }
+
+  onError(): RedisClientType {
+    return this.client.on("error", (err) =>
+      console.log("Redis Client Error", err)
+    );
+  }
+
+  async createIndex() {
+    try {
+      await this.client.ft.INFO("idx:products");
+    } catch (error) {
+      console.log("Index does not exist. Creating a new one...");
+      await this.client.ft.CREATE(
+        "idx:products",
+        {
+          ID: { type: SchemaFieldTypes.NUMERIC },
+          NAME: { type: SchemaFieldTypes.TEXT },
+          PRICE: { type: SchemaFieldTypes.NUMERIC },
+          DESCRIPTION: { type: SchemaFieldTypes.TEXT },
+        },
+        {
+          ON: "HASH",
+          PREFIX: "products:",
+        }
+      );
+    }
+  }
+
+  async searchAllProducts() {
+    return await this.client.ft.SEARCH("idx:products", "*");
+  }
+
+  async searchProductById(id: number | unknown) {
+    return await this.client.ft.SEARCH("idx:products", `@ID: [${id} ${id}]`);
+  }
+
+  async create(product: Product) {
+    await this.client.HSET(`products:${product.ID}`, product);
+  }
+
+  async update(product: Product | undefined) {
+    if (product)
+      await this.client.HSET(`products:${product.ID}`, product);
+  }
+
+  async delete(id: number | unknown) {
+    await this.client.DEL(`products:${id}`);
+  }
+
+  async synchronize() {
+    console.log("\nSynchronizing MySQL data to redis...");
+
+    await this.createIndex();
+
+    const products = await this.productRepository.getAll();
+    products.forEach(async (product) => {
+      await this.client.HSET(`products:${product.ID}`, product);
+    });
+
+    console.log("Synchronization completed.");
+  }
 }
-
-export { client };
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import express from "express";
 import { Request, Response, Router } from "express";
 import { ProductsRepository } from "./ProductsRepository";
 import { Product } from "./product";
-import { SchemaFieldTypes } from "redis";
-import { client } from "./redis";
+import { Redis } from "./Redis";
 
 const app = express();
 const port = 3000;
@@ -13,6 +12,7 @@ const routes = Router();
 app.use(express.json());
 
 const productsRepo = new ProductsRepository();
+const redis = new Redis();
 
 routes.get("/", (req: Request, res: Response) => {
   res.statusCode = 200;
@@ -20,10 +20,8 @@ routes.get("/", (req: Request, res: Response) => {
 });
 
 routes.get("/getAllProducts", async (req: Request, res: Response) => {
-  // obter todos os produtos.
-  const result = await client.ft.search("idx:products", "*");
-
-  //const products = await productsRepo.getAll();
+  await redis.synchronize();
+  const result = await redis.searchAllProducts();
 
   const products = result.documents;
   res.statusCode = 200;
@@ -32,16 +30,12 @@ routes.get("/getAllProducts", async (req: Request, res: Response) => {
 });
 
 routes.get("/getById/:id", async (rec: Request, res: Response) => {
+  await redis.synchronize();
+
   const id = rec.params.id as unknown as number;
-  const result = await client.HGETALL("products:" + id);
-  //console.log(JSON.stringify(result, null, 2))
-  //const result = await client.ft.search("idx:products", `@products: ${id}`);
+  const result = await redis.searchProductById(id);
 
-  const product = JSON.stringify(result, null, 2);
-
-  /* const products = await productsRepo.getById(
-    rec.params.id as unknown as number
-  ); */
+  const product = JSON.stringify(result.documents[0], null, 2);
 
   res.statusCode = 200;
   res.type("application/json");
@@ -49,58 +43,56 @@ routes.get("/getById/:id", async (rec: Request, res: Response) => {
 });
 
 routes.post("/create", async (rec: Request, res: Response) => {
+  await redis.synchronize();
+
   const newProduct = rec.body as Product;
   const products = await productsRepo.create(newProduct);
 
-  try {
-    const respReply = await client.ft.INFO("idx:products");
+  redis.create(products);
 
-    if (respReply.indexName == null) {
-      client.ft.create("idx:products", {
-        name: SchemaFieldTypes.TEXT,
-        price: SchemaFieldTypes.NUMERIC,
-        dscription: SchemaFieldTypes.TEXT,
-      });
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      if (e.message === "Index already exists")
-        console.log("Index exists already, skipped creation.");
-      else {
-        console.error(e);
-        process.exit(1);
-      }
-    }
-  }
-
-  client.hSet(`products:${products.ID}`, rec.body);
   res.statusCode = 200;
   res.type("application/json");
   res.send(products);
 });
 
 routes.post("/update", async (rec: Request, res: Response) => {
-  const updatedProduct = rec.body as Product;
+  try {
+    await redis.synchronize();
 
-  const redisProduct = {
-    name: updatedProduct.name,
-    price: updatedProduct.price,
-    description: updatedProduct.description,
-  };
+    const updatedProduct = rec.body as Product;
 
-  const products = await productsRepo.update(updatedProduct);
-  await client.hSet(`products:${updatedProduct.id}`, redisProduct);
+    if (!updatedProduct) {
+      res.statusCode = 400;
+      res.type("application/json");
+      res.send({ error: "Invalid product data." });
+    }
 
-  res.statusCode = 200;
-  res.type("application/json");
-  res.send(products);
+    const products = await productsRepo.update(updatedProduct);
+
+    if (!products) {
+      res.statusCode = 404;
+      res.type("application/json")
+      res.send({ error: "Product not found." });
+    }
+
+    redis.update(products);
+
+    res.statusCode = 200;
+    res.type("application/json");
+    res.send(products);
+  } catch (e) {
+    res.statusCode = 404;
+    res.send({ error: e})
+  }
 });
 
 routes.delete("/delete/:id", async (rec: Request, res: Response) => {
+  await redis.synchronize();
   const id = rec.params.id as unknown as number;
 
   await productsRepo.delete(id);
-  await client.del("products:" + id);
+
+  redis.delete(id);
 
   res.statusCode = 200;
   res.type("application/json");
@@ -110,17 +102,6 @@ routes.delete("/delete/:id", async (rec: Request, res: Response) => {
 // aplicar as rotas na aplicação web backend.
 app.use(routes);
 
-app.listen(3000, async () => {
+app.listen(port, async () => {
   console.log("Server is running on 3000");
-  console.log("\nSynchronizing MySQL data to redis...");
-
-  const products = await productsRepo.getAll();
-  products.forEach((product) => {
-    const name = product.NAME;
-    const price = product.PRICE;
-    const description = product.DESCRIPTION;
-    client.hSet(`products:${product.ID}`, { name, price, description });
-  });
-
-  console.log("Synchronization completed.");
 });
